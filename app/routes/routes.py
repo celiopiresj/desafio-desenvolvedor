1a972ba8acaccaed8d7e324c3ecd5335b97bfe55
Celio Pires Jr. <72475635+celiopiresj@users.noreply.github.com>
2024-09-29 15:04:49 -0300

    Endpoint de get_files_by_name

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, UploadFile, File
 from fastapi.responses import JSONResponse
-from services.file_service import save_file, get_files
+from services.file_service import save_file, get_files, get_files_by_name
 
 router = APIRouter()
 
@@ -13,3 +13,8 @@ async def upload_file(file: UploadFile = File(...)):
 async def list_files(page: int = 1, page_size: int = 10):
     results = await get_files(page, page_size)
     return {"files": results}
+
+@router.get("/files/filename/{filename}", status_code=200)
+async def get_file_by_filename(filename: str, page: int = 1, page_size: int = 10, paginate: bool = True, exact_match: bool = True):
+    results = await get_files_by_name(filename, page, page_size, paginate, exact_match)
+    return {"file": results}
--- app/services/file_service.py
+++ app/services/file_service.py
@@ -1,28 +1,35 @@
 from fastapi import UploadFile, HTTPException
-from io import StringIO, BytesIO 
+from io import StringIO, BytesIO
+from bson import Regex
 import pandas as pd
 from datetime import datetime
 from database.database import db
+
 
 def decode_content(contents: bytes) -> str:
     try:
         return contents.decode('utf-8')
     except UnicodeDecodeError:
         return contents.decode('ISO-8859-1')
-    
+
+
 async def check_file_exists(filename: str, db):
     if await db['files'].find_one({"Filename": filename}):
-        raise HTTPException(status_code=400, detail="O arquivo já foi enviado anteriormente.")
+        raise HTTPException(
+            status_code=400, detail="O arquivo já foi enviado anteriormente.")
+
 
 def parse_csv_content(decoded_content: str) -> pd.DataFrame:
     initial_lines = decoded_content.splitlines()[:2]
     header = 0 if "RptDt" in initial_lines[0] else 1
     return pd.read_csv(StringIO(decoded_content), sep=";", header=header, na_filter=False, low_memory=False)
 
+
 def parse_excel_content(file_content: bytes) -> pd.DataFrame:
     initial_lines = decode_content(file_content).splitlines()[:2]
     header = 0 if "RptDt" in initial_lines[0] else 1
     return pd.read_excel(BytesIO(file_content), header=header, engine='openpyxl')
+
 
 async def save_data_to_mongo(db, data: list):
     collection = db['files']
@@ -36,29 +43,28 @@ async def save_data_to_mongo(db, data: list):
 async def save_file(file: UploadFile):
     if not file:
         raise HTTPException(status_code=400, detail="Nenhum arquivo enviado.")
-    
-    # Verificar se o arquivo já foi enviado anteriormente
+
     await check_file_exists(file.filename, db)
 
-    # Leitura e decodificação do conteúdo do arquivo
     contents = await file.read()
     decoded_content = decode_content(contents)
 
-    # Processamento com base no tipo de arquivo
     if file.content_type == "text/csv":
         try:
             df = parse_csv_content(decoded_content)
         except pd.errors.ParserError as e:
-            raise HTTPException(status_code=400, detail=f"Erro ao analisar o arquivo CSV: {str(e)}")
-    elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": 
+            raise HTTPException(
+                status_code=400, detail=f"Erro ao analisar o arquivo CSV: {str(e)}")
+    elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
         try:
             df = parse_excel_content(contents)
         except Exception as e:
-            raise HTTPException(status_code=400, detail=f"Erro ao analisar o arquivo Excel: {str(e)}")
+            raise HTTPException(
+                status_code=400, detail=f"Erro ao analisar o arquivo Excel: {str(e)}")
     else:
-        raise HTTPException(status_code=400, detail="Formato de arquivo não suportado.")
-    
-   # Converte os dados e salva no MongoDB
+        raise HTTPException(
+            status_code=400, detail="Formato de arquivo não suportado.")
+
     df["Filename"] = file.filename
     df["Upload_date"] = datetime.now().strftime("%Y-%m-%d")
     data = df.to_dict(orient='records')
@@ -66,21 +72,23 @@ async def save_file(file: UploadFile):
         await save_data_to_mongo(db, data)
         return {"detail": "Arquivo enviado e dados salvos com sucesso."}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Erro ao salvar dados no MongoDB: {str(e)}")  
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao salvar dados no MongoDB: {str(e)}")
 
-async def paginate(page: int, page_size: int):
-    pipeline = [ 
-        {"$sort": {"RptDt": -1}},      
+
+async def paginate_files(page: int, page_size: int):
+    pipeline = [
+        {"$sort": {"RptDt": -1}},
         {"$skip": (page - 1) * page_size},
-        {"$limit": page_size},      
-        {"$project": {"_id": 0, "Filename": 0, "Upload_date": 0}} 
+        {"$limit": page_size},
+        {"$project": {"_id": 0, "Filename": 0, "Upload_date": 0}}
     ]
 
-    result =  db["files"].aggregate(pipeline)
+    result = db["files"].aggregate(pipeline)
 
-    total_documents = await  db["files"].count_documents({})
+    total_documents = await db["files"].count_documents({})
     total_pages = (total_documents + page_size - 1) // page_size
-    
+
     return {
         "current_page": page,
         "total_pages": total_pages,
@@ -88,10 +96,85 @@ async def paginate(page: int, page_size: int):
         "data": await result.to_list(length=page_size)
     }
 
+
+async def get_distinct_filenames(filename: str, exact_match: bool):
+    if exact_match:
+        return await db["files"].distinct("Filename", {"Filename": filename})
+    else:
+        regex = Regex(f"^{filename}", "i")
+        return await db["files"].distinct("Filename", {"Filename": {"$regex": regex}})
+
+
+async def paginate_file_by_name(filename: str, page: int, page_size: int, exact_match: bool):
+
+    names = await get_distinct_filenames(filename, exact_match)
+
+    files = []
+    for name in names:
+        pipeline = [
+            {"$match": {"Filename": name}},
+            {"$sort": {"RptDt": -1}},
+            {"$skip": (page - 1) * page_size},
+            {"$limit": page_size},
+            {"$project": {"_id": 0, "Upload_date": 0, "Filename": 0}}
+        ]
+
+        response = db["files"].aggregate(pipeline)
+        result = await response.to_list(length=page_size)
+
+        total_documents = await db["files"].count_documents({"Filename": name})
+        total_pages = (total_documents + page_size - 1) // page_size
+
+        files.append({"total_pages": total_pages,
+                     "filename": name, "data": result})
+
+    return {
+        "files_found": len(names),
+        "current_page": page,
+        "page_size": page_size,
+        "filename_search": filename,
+        "result": files
+    }
+
+
+async def filter_files_by_name(filename: str, exact_match: bool):
+
+    names = await get_distinct_filenames(filename, exact_match)
+
+    files = []
+    for name in names:
+        pipeline = [
+            {"$match": {"Filename": name}},
+            {"$sort": {"RptDt": -1}},
+            {"$project": {"_id": 0, "Upload_date": 0, "Filename": 0}}
+        ]
+
+        response = db["files"].aggregate(pipeline)
+        result = await response.to_list()
+
+        files.append({"filename": name, "data": result})
+
+    return {
+        "files_found": len(names),
+        "filename_search": filename,
+        "result": files
+    }
+
+
 async def get_files(page: int, page_size: int):
     try:
-        #paginar consulta
-        result = await paginate(page, page_size)
+        return await paginate_files(page, page_size)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+async def get_files_by_name(filename: str, page: int, page_size: int,  paginate: bool, exact_match: bool):
+    try:
+        if paginate:
+            result = await paginate_file_by_name(filename, page, page_size, exact_match)
+        else:
+            result = await filter_files_by_name(filename, exact_match)
+
         return result
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e)) 
+        raise HTTPException(status_code=500, detail=str(e))
